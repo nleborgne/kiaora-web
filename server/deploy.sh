824552538613d0fb906d19384e3c0565e4f5cfaa
Nicolas Le Borgne <nicolas.leborgne@me.com>
2021-11-04 15:56:22 +0100

    separated client and server

--- server/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo What should the version be?
-
-read VERSION
-docker build -t nicoolaslb/kiaora:$VERSION .
-docker push nicoolaslb/kiaora:$VERSION
-
-ssh root@147.182.209.193 "docker pull nicoolaslb/kiaora:$VERSION && docker tag nicoolaslb/kiaora:$VERSION dokku/api-kiaora:$VERSION && dokku tags:deploy api-kiaora $VERSION"
